1ad1d7bdf9f472f82b126878f4b0d85380806b54
“kumarraja.k” <kumarraja.k@apcfss.in>
2025-05-30 21:05:58 +0530

    gfbrfgbfgbfgb

--- src/components/AjaxURLs.jsx
+++ src/components/AjaxURLs.jsx
@@ -1,7 +1,7 @@
 export const API_URL = 'https://api.apnidhi.in/cfsshrms'
 export const LOGIN_API_URL = API_URL + '/api/auth/signin';
 export const FRS_URL = API_URL + '/employeeRegistration/attendanceSave';
-export const PROXY_URL = 'https://cors-anywhere.herokuapp.com';
+export const PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
 
 export function withProxy(url) {
     return PROXY_URL + url;
