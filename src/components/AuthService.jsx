e40f6d94756d2acb0df8dbde92a1d6ebed9d72b6
Jayakrishna <jayakrishna.b@apcfss.in>
2025-06-01 15:26:31 +0530

    my commit

--- src/components/AuthService.jsx
+++ src/components/AuthService.jsx
@@ -3,7 +3,18 @@ import { LOGIN_API_URL, PROXY_URL } from "./AjaxURLs";
 
 class AuthService {
     login(formData) {
-        let loginAxios = axios.create();
+        let loginAxios = axios.create({
+            headers: {
+                'authority': 'api.apnidhi.in',
+                'method': 'POST',
+                'path': '/cfsshrms/api/auth/signin',
+                'scheme': 'https',
+                'Content-Type': 'application/json',
+                'Accept': 'application/json, text/plain, */*',
+                'Origin': 'https://apcfssfrs.netlify.app/',
+                'Referer': 'https://api.apnidhi.in/cfsshrms',
+            }
+        });
         return loginAxios.post(LOGIN_API_URL, formData);
     }
 }
--- src/components/axiosLoaderServices.jsx
+++ src/components/axiosLoaderServices.jsx
@@ -1,8 +1,19 @@
 import axios from 'axios';
 import { failureResponse } from './commonFunctions';
 
-const axiosLoaderServices = axios.create();
-axiosLoaderServices.defaults.timeout=90000
+const axiosLoaderServices = axios.create({
+    headers: {
+        'authority': 'api.apnidhi.in',
+        'method': 'POST',
+        'path': '/cfsshrms/employeeRegistration/attendanceSave',
+        'scheme': 'https',
+        'Content-Type': 'application/json',
+        'Accept': 'application/json, text/plain, */*',
+        'Origin': 'https://apcfssfrs.netlify.app/',
+        'Referer': 'https://api.apnidhi.in/cfsshrms',
+    }
+});
+axiosLoaderServices.defaults.timeout = 90000
 axiosLoaderServices.interceptors.response.use(async (config) => {
     return config;
 }, function (error) {
@@ -13,7 +24,7 @@ axiosLoaderServices.interceptors.response.use(async (config) => {
 axiosLoaderServices.interceptors.request.use(async (config) => {
     const token = "Bearer" + ' ' + localStorage.getItem("token");
     config.headers.Authorization = token;
-    config.timeout=120000
+    config.timeout = 120000
     return config;
 }, function (error) {
     failureResponse(error)
